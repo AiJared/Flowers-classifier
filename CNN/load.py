5e1e24b380b17d23f0082c35654a4f878f522cd3
Kerriea-star <ojared511@gmail.com>
2023-01-28 11:51:39 +0300

    fixes

--- CNN/load.py
+++ CNN/load.py
@@ -18,45 +18,45 @@ labels=[]
 # ----------------
 
 # Daisy 0
-daisy = os.listdir(os.getcwd() + "C:\Users\ojare\.keras\datasets\flower_photos\daisy")
+daisy = os.listdir(os.getcwd() + "/CNN/data/daisy")
 for x in daisy:
-    imag=cv2.imread(os.getcwd() + "C:\Users\ojare\.keras\datasets\flower_photos\daisy" + x)
+    imag=cv2.imread(os.getcwd() + "/CNN/data/daisy" + x)
     img_from_ar = Image.fromarray(imag, 'RGB')
     resized_image = img_from_ar.resize((180, 180))
     data.append(np.array(resized_image))
     labels.append(0)
 
 # Dandeliion 1
-dandenion = os.listdir(os.getcwd() + "C:\Users\ojare\.keras\datasets\flower_photos\dandelion")
+dandenion = os.listdir(os.getcwd() + "/CNN/data/dandelion")
 for x in dandenion:
-    imag=cv2.imread(os.getcwd() + "C:\Users\ojare\.keras\datasets\flower_photos\dandelion" + x)
+    imag=cv2.imread(os.getcwd() + "/CNN/data/dandelion" + x)
     img_from_ar = Image.fromarray(imag, 'RGB')
     resized_image = img_from_ar.resize((180, 180))
     data.append(np.array(resized_image))
     labels.append(1)
 
 # Roses 2
-roses = os.listdir(os.getcwd() + "C:\Users\ojare\.keras\datasets\flower_photos\roses")
+roses = os.listdir(os.getcwd() + "/CNN/data/roses")
 for x in roses:
-    imag=cv2.imread(os.getcwd() + "C:\Users\ojare\.keras\datasets\flower_photos\roses" + x)
+    imag=cv2.imread(os.getcwd() + "/CNN/data/roses" + x)
     img_from_ar = Image.fromarray(imag, 'RGB')
     resized_image = img_from_ar.resize((180, 180))
     data.append(np.array(resized_image))
     labels.append(2)
 
 # Sunflowers 3
-sunflowers = os.listdir(os.getcwd() + "C:\Users\ojare\.keras\datasets\flower_photos\sunflowers")
+sunflowers = os.listdir(os.getcwd() + "/CNN/data/sunflowers")
 for x in sunflowers:
-    imag=cv2.imread(os.getcwd() + "C:\Users\ojare\.keras\datasets\flower_photos\sunflowers" + x)
+    imag=cv2.imread(os.getcwd() + "/CNN/data/sunflowers" + x)
     img_from_ar = Image.fromarray(imag, 'RGB')
     resized_image = img_from_ar.resize((180, 180))
     data.append(np.array(resized_image))
     labels.append(3)
 
 # Tulips 4
-tulips = os.listdir(os.getcwd() + "C:\Users\ojare\.keras\datasets\flower_photos\tulips")
+tulips = os.listdir(os.getcwd() + "/CNN/data/tulips")
 for x in tulips:
-    imag=cv2.imread(os.getcwd() + "C:\Users\ojare\.keras\datasets\flower_photos\tulips" + x)
+    imag=cv2.imread(os.getcwd() + "/CNN/data/tulips" + x)
     img_from_ar = Image.fromarray(imag, 'RGB')
     resized_image = img_from_ar.resize((180, 180))
     data.append(np.array(resized_image))
