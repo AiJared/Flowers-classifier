81bf58430c3c70a88507f738b0ae526507757eca
Kerriea-star <ojared511@gmail.com>
2023-02-01 07:41:04 +0300

    fixes

--- flowers/settings.py
+++ flowers/settings.py
@@ -3,13 +3,14 @@ import os
 from pathlib import Path
 
 import environ
+import dj_database_url
 
 env = environ.Env()
 environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+DATABASE_URL = ""
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
