7276565802da62e1bdb2eb9f006dbc6714ab25a5
Kerriea-star <ojared511@gmail.com>
2023-02-06 06:52:45 +0300

    application fixes

--- flowers/urls.py
+++ flowers/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path
-from .views import index
+from classifier.views import index
 
 urlpatterns = [
     path("", index, name="index"),
