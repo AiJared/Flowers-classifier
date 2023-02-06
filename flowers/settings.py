29e25d751e1ef64144cb1728ff128843f276e723
Kerriea-star <ojared511@gmail.com>
2023-02-06 06:36:47 +0300

    static files fixes

--- flowers/settings.py
+++ flowers/settings.py
@@ -101,16 +101,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
-MEDIA_URL = "/media/"
-
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    BASE_DIR / "assets",
+    os.path.join(BASE_DIR, 'static'),
 ]
 
-MEDIA_ROOT = BASE_DIR / "media"
-STATIC_ROOT = BASE_DIR / "static"
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
